b5dcbf2918a71835d878ac356ebaf5df842433c8
Saurav Kumar <saurav.primary@gmail.com>
2021-02-12 19:40:14 +0530

    adding edit and delete button

--- src/app.js
+++ src/app.js
@@ -8,11 +8,11 @@ const axios = require('axios')
 
 const app = express()
 const port = process.env.PORT || 3000
+const resourcePath = path.join(__dirname,'../resource')
 
 app.use(cors());
 app.use(bodyParser.json()) 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use('/bootstrap',express.static((__dirname+ '/node_modules/bootstrap/dist')))
 
 // PATH FOR DIFFERENT RESOURCES
 const viewsPath = path.join(__dirname,'../template/views')
@@ -23,6 +23,7 @@ const partialsPath = path.join(__dirname,'../template/partials')
 app.set( 'view engine', 'hbs')
 app.set( 'views', viewsPath)
 hbs.registerPartials(partialsPath)
+app.use( express.static(resourcePath))
 
 app.get('/',async (req, res) =>{
     const response = await axios.get('https://memestreambackend.herokuapp.com/memes')
@@ -41,7 +42,7 @@ app.get('/',async (req, res) =>{
 app.post('/',(req,res)=>{
     var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
     var date = new Date(Date.now())
-    var date = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
+    var date = `Posted on ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
     var data = {
         name : req.body.name,
         caption : req.body.caption,
@@ -53,6 +54,53 @@ app.post('/',(req,res)=>{
     axios({
         method: "post",
         url: 'https://memestreambackend.herokuapp.com/memes',  
+        data : data
+    })
+    .then( response => console.log(response))
+    .catch( err => console.log(err.response))
+
+    res.redirect('/')
+})
+app.post('/memes/delete/:id',async (req,res)=>{
+    var data = {
+        id : req.params.id
+    }
+    var returndata
+    await axios({
+        method: "post",
+        url: 'https://memestreambackend.herokuapp.com/memes/delete',  
+        data : data
+    })
+    .then( response => returndata = response)
+    .catch( err => console.log(err.response))
+
+    res.redirect('/')
+})
+
+app.post('/memes/edit/:id',async (req,res)=>{
+    
+    const response = await axios.get('https://memestreambackend.herokuapp.com/memes/'+req.params.id)
+    const data = await response.data
+    //console.log(data)
+    res.render('updatepage', data[0])
+})
+
+app.post('/updatememe',async (req,res)=>{
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+    var date = new Date(Date.now())
+    var date = `Updated on ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
+    var data = {
+        id : req.body.id,
+        name : req.body.name,
+        caption : req.body.caption,
+        imgURL : req.body.imgURL,
+        date : date
+    }
+    //console.log(data)
+
+    axios({
+        method: "post",
+        url: 'https://memestreambackend.herokuapp.com/memes/edit',  
         data : data
     })
     .then( response => console.log(response))
